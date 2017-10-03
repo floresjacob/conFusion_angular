86a3488efd5b483f791ab56ee37f8779ada5b613
Jacob Flores <jacob.e.flores562@gmail.com>
2017-10-02 19:08:18 -0700

    Components pt.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import { MatSelectModule, MatListModule, MatGridListModule , MatInputModule, MatSlideToggleModule ,
+MatToolbarModule , MatDialogModule , MatCardModule , MatButtonModule , MatSliderModule } from '@angular/material';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
+import 'hammerjs';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpModule,
+    MatSelectModule, MatListModule, MatGridListModule , MatInputModule, MatSlideToggleModule ,
+    MatToolbarModule , MatDialogModule , MatCardModule , MatButtonModule , MatSliderModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
