6198725e2920d39c361de576402f30e777ed4bf0
Jacob Flores <jacob.e.flores562@gmail.com>
2017-10-18 22:57:17 -0700

    Single Page Applications Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { Promotion } from './shared/promotion';
 
 @NgModule({
   declarations: [
@@ -39,7 +40,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, Promotion],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
