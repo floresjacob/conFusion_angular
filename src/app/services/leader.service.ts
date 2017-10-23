2d0f46b5fedb72bb7e4524bdf6af09ab50fac0be
Jacob Flores <jacob.e.flores562@gmail.com>
2017-10-22 20:11:40 -0700

    Promise Part 2

--- src/app/services/leader.service.ts
+++ src/app/services/leader.service.ts
@@ -7,19 +7,28 @@ export class LeaderService {
 
   constructor() { }
   getLeaders(): Promise<Leader[]> {
-    return Promise.resolve(LEADERS);
+    return new Promise(resolve=> {
+      // Simulate server latency with 2 second delay
+        setTimeout(() => resolve(LEADERS), 2000);
+    });
   }
 
   getLeader(id: number): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leader) => (leader.id === id))[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leader) => (leader.id === id))[0]), 2000);
+    });
   }
 
   getFeaturedLeader(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leaders) => leaders.featured)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leaders) => leaders.featured)[0]), 2000);
+    });
   }
 
   getLeaderAbbr(): Promise<Leader> {
-    return Promise.resolve(LEADERS.filter((leaders) => leaders.abbr)[0]);
+    return new Promise(resolve => {
+      setTimeout(() => resolve(LEADERS.filter((leaders) => leaders.abbr)[0]), 2000);
+    });
   }
 
 }
